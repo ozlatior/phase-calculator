2db357666da4571a4358f98082b4c47ae9d6ef86
Octav Zlatior <octav.zlatior@gmail.com>
2020-06-14 23:18:38 +0800

    Adds index.js file

---
+++ index.js
@@ -0,0 +1,13 @@
+const Phases = require("./src/phases.js");
+
+const data = require("./objects/jupiter.json");
+
+const TimeOne = require("./src/time-one.js");
+const Phases = require("./src/phases.js");
+const Environment = require("./src/environment.js");
+
+module.exports = {
+	Environment: Environment,
+	Phases: Phases,
+	TimeOne: TimeOne
+};
